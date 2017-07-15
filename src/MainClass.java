6eb5376203d8e3ab0cf07d5199183fcaba389419
arkan382382 <arkan382382@outlook.com>
2017-07-15 09:55:46 +0200

    Object plan

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,20 +1,43 @@
 import javax.imageio.ImageIO;
+import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by arkan on 14.07.2017.
  * pobieranie i wyswietlanie obrazka: https://javastart.pl/static/grafika_awt_swing/pobieranie-i-wyswietlanie-obrazow/
  */
 public class MainClass {
-    public static void main(String[] argc){
+    public static void main(String[] argc) {
         System.out.println("test");
+        StorageOfSmallPictures smallPictures = new StorageOfSmallPictures();
+        smallPictures.readPicturesFromDirectory();
+        System.out.println("small pictures: " + smallPictures.getNumberOfLoadedPhotos());
 
-        int width = 200;
+        FinalPicture finallPicture = new FinalPicture();
+        finallPicture.setSizeOfFinalPicture(smallPictures.getNumberOfLoadedPhotos());
+
+    /*    int width = 200;
         int height = 150;
         BufferedImage image = null;
+        File fileActual = null;
+        ArrayList<File> fileTemp = new ArrayList<File>();
+        try{
+            fileActual = new File("D:\\test.jpg");
+            image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+            image = ImageIO.read(fileActual);
+            fileTemp.add(fileActual);
+            fileTemp.add(fileActual);
+        } catch (IOException e) {
+            System.out.println("Error: " + e);
+        }
+
+        System.out.println("size: " + fileTemp.size());
         File file = null;
 
         Image[] temp = new Image[10]; // or list of image (try to use different structure)
@@ -27,6 +50,18 @@ public class MainClass {
         }catch(IOException e){
             System.out.println("Error: " + e);
         }
+*/
 
-    }
-}
+  /*      File directory = new File("D:\\zrodlo\\");
+        Map<String, ImageIcon> iconMap = new HashMap<String, ImageIcon>();
+        for (File file : directory.listFiles()) {
+            if (file.getName().toLowerCase().endsWith(".jpg")) {
+                iconMap.put(file.getName(), new ImageIcon(file.getPath()));
+            }
+        }
+*/
+      //  System.out.println(iconMap.size());
+
+
+
+    }}
--- src/StorageOfSmallPictures.java
+++ src/StorageOfSmallPictures.java
@@ -1,38 +1,54 @@
 import javax.imageio.ImageIO;
+import javax.swing.*;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by arkan on 14.07.2017.
  */
 
-public class StorageOfSmallPictures{
+public class StorageOfSmallPictures {
     private int numberOfLoadedPictures = 0;
-/*
-    int width = 200;
-    int height = 150;
-    BufferedImage image = null;
-    File file = null;
+    /*
+        int width = 200;
+        int height = 150;
+        BufferedImage image = null;
+        File file = null;
 
-    try{
-        file = new File("D:\\test.jpg");
-        image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-        image = ImageIO.read(file);
-        System.out.println("Reading complete.");
-    }catch(IOException e){
-        System.out.println("Error: " + e);
+        try{
+            file = new File("D:\\test.jpg");
+            image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+            image = ImageIO.read(file);
+            System.out.println("Reading complete.");
+        }catch(IOException e){
+            System.out.println("Error: " + e);
+        }
+    */
+    Map<String, ImageIcon> iconMap = new HashMap<String, ImageIcon>();
+
+    public void readPicturesFromDirectory() {
+        File directory = new File("D:\\zrodlo\\");
+
+        for (File file : directory.listFiles()) {
+            if (file.getName().toLowerCase().endsWith(".jpg")) {
+                iconMap.put(file.getName(), new ImageIcon(file.getPath()));
+            }
+        }
+        this.setNumberOfLoadedPictures();
     }
-*/
-    public int howManyPictures(){
-        /*
-        *for loop, value in loop, has next,
-        *
-         */
-        return 0;
+
+    /*   public void setNumberOfLoadedPictures(int numberOfLoadedPictures) {
+           this.numberOfLoadedPictures = howManyPictures();
+       }   */
+    private void setNumberOfLoadedPictures() {
+        numberOfLoadedPictures = iconMap.size();
     }
-    public int getNumberOfLoadedPhotos(){
-        return this.numberOfLoadedPictures;
+
+    public int getNumberOfLoadedPhotos() {
+        return numberOfLoadedPictures;
     }
 }
